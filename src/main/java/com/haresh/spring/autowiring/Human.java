d3e88a53c37ab7dade3f0069796ba53398817f9c
Haresh Prajapati <hp494343@gmail.com>
2022-10-20 10:22:54 +0530

    Learn the qualifier annotation

--- src/main/java/com/haresh/spring/autowiring/Human.java
+++ src/main/java/com/haresh/spring/autowiring/Human.java
@@ -1,10 +1,12 @@
 package com.haresh.spring.autowiring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
 
     @Autowired
+    @Qualifier("humanHeart")
     private Heart heart;
 
 
